e2f8e836139e3743b4dfd087fd29ffdb8971bd92
mdg357 <mdg357@gmail.com>
2016-09-28 17:19:43 -0500

    More work on unit tests

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+
 import { ClockDateComponent } from './clock-date.component';
 import { ClockTimeComponent } from './clock-time.component';
 import { WeatherCurrentComponent } from './weather-current.component';
